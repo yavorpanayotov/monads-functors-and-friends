04c4b5a767e1832881c1da3387805ae717b72624
Yavor Panayotov <y.panayotov@yahoo.com>
2018-05-17 22:33:18 +0100

    clean up tests a bit

--- src/test/kotlin/root_k/functor/FunctorTest.kt
+++ src/test/kotlin/root_k/functor/FunctorTest.kt
@@ -1,6 +1,7 @@
 package root_k.functor
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import root_k.functor.Maybe.Just
 import root_k.functor.Maybe.None
@@ -8,16 +9,12 @@ import root_k.functor.Maybe.None
 class FunctorTest {
 
     @Test
-    fun fmapOnNothing_IsVoid() {
-        assertEquals(None, None `fmap` plus10())
+    fun `flat mapping nothing has no effect`() {
+        assertEquals(None, None fmap { fail<Unit>("should not be applied")})
     }
 
     @Test
-    fun fmapOnJust_MapsFunctionToInternalValue() {
-        assertEquals(Just(15), Just(5) `fmap` plus10())
-    }
-
-    private fun plus10(): (Int) -> Int {
-        return { it + 10 }
+    fun `fmap applies function on Just`() {
+        assertEquals(Just(15), Just(5) fmap {it + 10})
     }
 }
